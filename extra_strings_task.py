d3e6aa6edb5eae23a917c43471ceda3dd015a9bd
mosehgathigi <mosehgathigi@gmail.com>
2024-10-17 16:31:27 +0300

    second commit

---
+++ extra_strings_task.py
@@ -0,0 +1,45 @@
+firstName = 'Moses'
+lastName = 'Gathigi'
+
+fullName  =  firstName + ' ' + lastName
+print(fullName)
+
+# 2
+word = 'Python'
+print(word[0])
+print(word[-1])
+
+# 3
+phrase  = 'Learning Python is fun!'
+print(phrase[9:15])
+
+# 4
+text =  'reverse this'
+print(text[::-1])
+
+# 5
+greeting='Hello world'
+print(greeting.replace('world','Python'))
+
+# 6
+msg =  'Python Programming'
+print(msg.lower())
+print(msg.upper())
+
+# 7
+sentense = 'This is a simple sentense.'
+print(sentense.count('s'))
+
+# 8
+quote = 'The best way to predict  the future is to create it.'
+print('future' in quote)
+
+
+# 9
+description = 'data science'
+print(len(description))
+
+
+# 10
+name = '    John  Doe    '
+print(name.strip())
---
+++ int_floats.py
@@ -0,0 +1,14 @@
+num1 =65
+num2=56
+print(num1/num2)
+
+print(num1*num2)
+
+print(num1+num2)
+
+print(num1-num2)
+
+print(num1//num2)
+
+print(num1**num2)
+
